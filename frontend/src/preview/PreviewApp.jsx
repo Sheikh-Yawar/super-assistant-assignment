3d3151f191fb0a9be9490ee80a0178c7374b020c
Sheikh-Yawar <sheikhyawar95642@gmail.com>
2023-08-06 01:10:01 +0530

    Added redirects

--- frontend/src/preview/PreviewApp.jsx
+++ frontend/src/preview/PreviewApp.jsx
@@ -12,9 +12,8 @@ import "./PreviewApp.css";
 
 function PreviewApp({ data, headerData }) {
 	const [isLoading, setIsLoading] = useState(false);
-
 	useEffect(() => {
-		if (data.length === 1) {
+		if (data.length === 0) {
 			setIsLoading(true);
 		} else {
 			setIsLoading(false);
